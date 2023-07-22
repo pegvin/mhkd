d974ec4248541eb6a815d535f2edfe1a14452bae
pegvin <75035219+pegvin@users.noreply.github.com>
2023-07-23 00:59:30 +0530

    Comment Out Default Commands

--- config.def.h
+++ config.def.h
@@ -1,15 +1,15 @@
-/* This file is an example. You must configure this yourself. */
+#include <X11/XF86keysym.h>
 
 bool debug_print_spawned_command = false;
 
 struct MouseEvent mouse_events[] = {
-    { ButtonPress,   Mod4Mask, Button1, IgnoreMouseInfo, "katriac client_move_mouse down" },
-    { MotionNotify,  Mod4Mask, Button1, IgnoreMouseInfo, "katriac client_move_mouse motion" },
-    { ButtonRelease, Mod4Mask, Button1, IgnoreMouseInfo, "katriac client_move_mouse up" },
+    // { ButtonPress,   Mod4Mask, Button1, IgnoreMouseInfo, "do_something down" },
+    // { MotionNotify,  Mod4Mask, Button1, IgnoreMouseInfo, "do_something move" },
+    // { ButtonRelease, Mod4Mask, Button1, IgnoreMouseInfo, "do_something up" },
 
-    { ButtonPress,   Mod4Mask, Button3, IgnoreMouseInfo, "katriac client_resize_mouse down" },
-    { MotionNotify,  Mod4Mask, Button3, IgnoreMouseInfo, "katriac client_resize_mouse motion" },
-    { ButtonRelease, Mod4Mask, Button3, IgnoreMouseInfo, "katriac client_resize_mouse up" },
+    // { ButtonPress,   Mod4Mask, Button3, IgnoreMouseInfo, "do_something down" },
+    // { MotionNotify,  Mod4Mask, Button3, IgnoreMouseInfo, "do_something move" },
+    // { ButtonRelease, Mod4Mask, Button3, IgnoreMouseInfo, "do_something up" },
 
     /* When using AppendMouseInfo, additional information will be
      * appended to your command. For ButtonPress and ButtonRelease,
@@ -31,21 +31,16 @@ struct MouseEvent mouse_events[] = {
      * greater than what you have seen before.
      *
      * This serial number is a uint32_t. */
-    { ButtonPress,   Mod4Mask, Button2, AppendMouseInfo, "something_mouse down" },
-    { MotionNotify,  Mod4Mask, Button2, AppendMouseInfo, "something_mouse move" },
-    { ButtonRelease, Mod4Mask, Button2, AppendMouseInfo, "something_mouse up" },
+    // { ButtonPress,   Mod4Mask, Button2, AppendMouseInfo, "something_mouse down" },
+    // { MotionNotify,  Mod4Mask, Button2, AppendMouseInfo, "something_mouse move" },
+    // { ButtonRelease, Mod4Mask, Button2, AppendMouseInfo, "something_mouse up" },
+    { 0, 0, 0, 0, NULL }
 };
 
 struct KeyEvent key_events[] = {
-    /* katriawm */
-    { KeyPress, Mod4Mask, XK_j, "katriac client_select next" },
-    { KeyPress, Mod4Mask, XK_k, "katriac client_select prev" },
-    { KeyPress, Mod4Mask, XK_o, "katriac client_select recent" },
-
-    /* Applications */
-    { KeyPress, Mod4Mask, XK_Return, "terminal-wrapper" },
-    { KeyPress, Mod4Mask, XK_space, "browser-inputline" },
-    { KeyPress, Mod4Mask | ShiftMask, XK_c, "browser-clipboard-wrapper" },
-    { KeyPress, Mod4Mask | Mod1Mask | ShiftMask, XK_y, "jslock" },
+    // { KeyPress, 0, XF86XK_AudioLowerVolume, "bash ~/.local/bin/volume down" },
+    // { KeyPress, 0, XF86XK_AudioMute, "bash ~/.local/bin/volume toggle" },
+    // { KeyPress, 0, XF86XK_AudioRaiseVolume, "bash ~/.local/bin/volume up" },
+    { 0, 0, 0, NULL },
 };
 
